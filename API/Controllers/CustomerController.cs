7f555ef380d928cbed1eae107e8c1486ec69586d
Alexander Escobar <alex.escobar104@hotmail.com>
2023-05-16 16:57:15 -0500

    Agregar archivos de proyecto.

---
+++ API/Controllers/CustomerController.cs
@@ -0,0 +1,113 @@
+﻿using Business;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CustomerEntity = DataAccess.Data.Customer;
+using PostEntity = DataAccess.Data.Post;
+
+namespace API.Controllers.Customer
+{
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private BaseService<CustomerEntity> CustomerService;
+        private BaseService<PostEntity> PostService;
+        public CustomerController(BaseService<CustomerEntity> customerService, BaseService<PostEntity> postService)
+        {
+            CustomerService = customerService;
+            PostService = postService;
+        }
+        /// <summary>
+        /// metodo para retonar una sola entidad por el id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet()]
+        [Route("get")]
+        public CustomerEntity FindById(int id)
+        {
+            return CustomerService.FindById(id);
+        }
+
+        /// <summary>
+        /// metodo que retorna todas las entidades
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        [Route("list")]
+        public IQueryable<CustomerEntity> GetAll()
+        {
+            return CustomerService.GetAll();
+        }
+
+        /// <summary>
+        /// metodo para crear la entidad
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public CustomerEntity Create([FromBodyAttribute] CustomerEntity entity)
+        {
+            return CreateCustomer(entity);
+        }
+
+        private CustomerEntity CreateCustomer(CustomerEntity entity)
+        {
+            bool existe  = false;
+            //validar que no haya un usuario con el mismo nombre
+            List<CustomerEntity> customers = new List<CustomerEntity>(CustomerService.GetAll());
+            foreach (CustomerEntity c in customers)
+            {
+                if(entity.Name == c.Name)
+                {
+                    existe = true;
+                }
+            }
+
+            if (!existe)
+            {
+                return CustomerService.Create(entity);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// metodo para actualizar la entidad 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPut()]
+        public CustomerEntity Update(CustomerEntity entity)
+        {
+            return CustomerService.Update(entity.CustomerId, entity, out bool changed);
+        }
+
+        /// <summary>
+        /// metodo para eliminar la entidad 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpDelete()]
+        public CustomerEntity Delete([FromBodyAttribute] CustomerEntity entity)
+        {
+            //elimina todos los Post asociados a la entidad
+            List<PostEntity> customers = new List<PostEntity>(PostService.GetAll().ToList());
+            foreach (PostEntity p in customers)
+            {
+                if (entity.CustomerId == p.CustomerId)
+                {
+                    PostService.Delete(p);
+                }
+            }
+            return CustomerService.Delete(entity);
+        }
+    }
+}
---
+++ API/Controllers/PostController.cs
@@ -0,0 +1,94 @@
+﻿using Business;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PostEntity = DataAccess.Data.Post;
+using CustomerEntity = DataAccess.Data.Customer;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace API.Controllers.Post
+{
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        private BaseService<PostEntity> PostService;
+        private BaseService<CustomerEntity> CustomerService;
+        public PostController(BaseService<PostEntity> postService, BaseService<CustomerEntity> customerService)
+        {
+            PostService = postService;
+            CustomerService = customerService;
+        }
+
+        /// <summary>
+        /// metodo para traer todos los post 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public IQueryable<PostEntity> GetAll()
+        {
+            return PostService.GetAll();
+        }
+
+        /// <summary>
+        /// metodo para crera el post
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public PostEntity Create([FromBodyAttribute]  PostEntity entity)
+        {
+
+            //valida el body para cortar a 97 caracteres
+            if (entity.Body.Length > 200)
+            {
+                entity.Body = entity.Body.Substring(1,97) + "...";
+            }
+            //valida  el tipo de categoria
+            if (entity.Category == "Farándula")
+            {
+                entity.Type = 1;
+            }else if(entity.Category == "Política")
+            {
+                entity.Type = 2;
+            }
+            else if (entity.Category == "Futbol")
+            {
+                entity.Type = 3;
+            }
+
+            //valida Que el usuario asociado si exista 
+            CustomerEntity customer = new CustomerEntity();
+            customer =  CustomerService.FindById(entity.CustomerId);
+            
+            if(customer == null)
+            {
+                return null;
+            }
+            
+            return PostService.Create(entity);
+        }
+
+        /// <summary>
+        /// metodo para actualizar el post 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpPut()]
+        public PostEntity Update([FromBodyAttribute] PostEntity entity)
+        {
+            return PostService.Update(entity.PostId, entity, out bool changed);
+        }
+
+        /// <summary>
+        ///metodo para eliminar el post
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        [HttpDelete()]
+        public PostEntity Delete([FromBodyAttribute] PostEntity entity)
+        {
+            return PostService.Delete(entity);
+        }
+
+
+    }
+}
